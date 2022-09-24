462798b2b1302645e2df4d74ae1e7d1aae54f1e8
Miras.Ramazanov <mirasafa.lol@gmail.com>
2022-09-24 14:44:55 +0600

    apk

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -7,7 +7,5 @@ const Color tdwhite = Color(0xFFFFFFFF);
 const Color tdBlack = Color(0xFF3A3A3A);
 const Color tdGrey = Color(0xFF717171);
 
-//const Color tdBGColor = Color(0xFFEEEFF5);
-
 const Color tdBGColor = Color.fromARGB(253, 48, 48, 52);
 const Color tdTdColor = Color.fromARGB(251, 102, 102, 114);
